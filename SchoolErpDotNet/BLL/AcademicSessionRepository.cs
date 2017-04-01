70b9a18dd8989a48c6ee094bbb9f2ee6494eec7f
Ofemino <ofemino@gmail.com>
2017-04-01 17:18:03 +0100

    Added acadsession repo
    Added acadsession repo

---
+++ SchoolErpDotNet/BLL/AcademicSessionRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace BLL
+{
+    public class AcademicSessionRepository:BaseRepository<AcademicSession>
+    {
+        public IQueryable<AcademicSession> GetAllSession()
+        {
+            return GetAll();
+        }
+
+
+
+    }
+}
